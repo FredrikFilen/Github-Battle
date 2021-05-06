4b9f344e22a8e32c0202e5c398416ba4e5e98024
FredrikFilen <69147419+FredrikFilen@users.noreply.github.com>
2021-05-06 14:31:04 +0200

    asd

--- react-course/src/index.js
+++ react-course/src/index.js
@@ -1,42 +1,67 @@
 import React from "react";
 import ReactDom from "react-dom";
+
+//css
 import "./index.css";
+
+//setup vars
+
+const books = [
+  {
+    id: 1,
+    img:
+      "https://i.pinimg.com/564x/6a/33/09/6a330911364022ab242e42a79959cfc5.jpg",
+    title: "Meow Meow Mothafucka",
+    author: "Mister Meow",
+  },
+
+  {
+    id: 2,
+    img: "https://i.ytimg.com/vi/ndsaoMFz9J4/hq720.jpg",
+    title: "hurrrrrr",
+    author: "katt",
+  },
+  {
+    id: 3,
+    img: "https://i.ytimg.com/vi/ndsaoMFz9J4/hq720.jpg",
+    title: "hurrrrrr",
+    author: "katt",
+  },
+];
 
 function BookList() {
   return (
-    <section classname="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+    <section className="booklist">
+      {books.map((book) => {
+        const { img, title, author } = book;
+        return <Book key={book.id} {...book}></Book>;
+      })}
     </section>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
+  const { img, author, title } = props;
+
+  //attribute, eventhandler
+  const clickHandler = () => {
+    alert("asdasd");
+  };
   return (
-    <article classname="book">
-      <Image></Image>
-      <Title></Title>
-      <Author></Author>
+    <article
+      className="book"
+      onMouseOver={() => {
+        console.log(title);
+      }}
+    >
+      <img src={img} alt="" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      <button type="button" onClick={clickHandler}>
+        click me
+      </button>
     </article>
   );
 };
-
-const Image = () => {
-  return (
-    <img
-      src="https://i.pinimg.com/564x/6a/33/09/6a330911364022ab242e42a79959cfc5.jpg"
-      alt=""
-    />
-  );
-};
-
-const Title = () => {
-  return <h1>meow</h1>;
-};
-
-const Author = () => <h4>mister meow</h4>;
 
 ReactDom.render(<BookList />, document.getElementById("root"));
